3fa1136493a59afb15e7f0407ea170aebc9c6ded
Manal Mehdaoui <115832170+Mino100@users.noreply.github.com>
2023-05-25 20:16:20 +0100

    'changes'

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -86,7 +86,16 @@ function Projects() {
               demoLink="https://youtu.be/nGs_PFEz0us"
             />
           </Col>
-      
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={snake}
+              isBlog={false}
+              title="The Snake game"
+              description="The Snake Game in Unity using C# is currently under development. Players will soon be able to control a snake, collect food, and avoid obstacles to grow longer. Exciting features like power-ups, different levels, and a high-score system are being implemented. Stay tuned for updates as the game progresses towards completion."
+              ghLink="https://github.com/Manal1110/Snake-game"
+              demoLink="https://youtu.be/COaZnOneCvs"
+            />
+          </Col>
 
 
           
